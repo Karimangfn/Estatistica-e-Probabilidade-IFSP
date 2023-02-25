08f5a11793af7ce9c7806e5381a73b2e709519ea
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:17:40 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 3/Aula 3 - Operadores Matematicos.R
@@ -0,0 +1,49 @@
+#Soma
+5 + 5
+
+#Subtração
+10 - 8
+
+#Divisão
+9 / 3
+
+#Multiplicação
+4 * 2
+
+#Potencia
+4 ** 2
+4 ^ 2
+
+#Comparação
+5 == 6
+5 == 5
+
+#Diferente
+5 != 6
+5 != 5
+
+#Maior
+4 > 2
+
+#Menor
+3 < 2
+
+#Maior e Menor Igual
+4 >= 5
+5 <= 4
+
+#AND
+6 == 6 & 7 == 7
+6 == 6 & 7 != 5
+
+#OR
+6 == 6 | 7 == 7
+6 == 6 | 7 != 5
+
+#Negação
+!6 == 6
+!4 > 2
+
+
+
+
