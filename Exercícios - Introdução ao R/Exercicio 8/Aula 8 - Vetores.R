b0ad18a124744a6aeb5831da1c66886636909026
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:20:19 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 8/Aula 8 - Vetores.R
@@ -0,0 +1,20 @@
+#Vetores - Estrutura básica de dados
+#Uma sequencia de dados do mesmo tipo
+
+#Vetor de Caracteres
+Nomes <- c("Fernando","Maria","Reginaldo")
+
+#Função is.vector confere se a variavel é um vetor
+is.vector(Nomes) 
+mode(Nomes)
+
+#Vetor Numérico
+Numeros <- c("100","200","300")
+is.vector(Numeros)
+mode(Numeros)
+
+#Vetor Lógico
+L1 <- TRUE
+is.vector(L1)
+mode(L1)
+
