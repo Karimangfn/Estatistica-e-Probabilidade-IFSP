9cddaa1be2c38443ec3ae59ccbce7869406151f2
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:17:16 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 15/Aula 15 - Funções.R
@@ -0,0 +1,52 @@
+#Funções
+
+a <- c(100,200,300,400)
+
+#Soma dos valores de "a"
+x <- 0 
+for (i in a) {
+  x <- x + i
+}
+print(x)
+
+b <- c(50,60,70,80)
+
+#Soma dos valores de "b"
+x <- 0 
+for (i in b) {
+  x <- x + i
+}
+print(x)
+
+#Função para realizar somas 
+
+soma <- function (y) {
+  x <- 0
+  for (i in y) {
+    x <- x + i
+  }
+  print(x)
+}
+
+#Usando a função
+soma(a)
+soma(b)
+
+#Função que recebe 2 parâmetros
+soma2 <- function (y,z) {
+  x <- 0
+  c <- 0
+  for (i in y) {
+    c <- c + 1
+    x <- z[c] + i
+    print(x)
+  }
+}
+
+soma2(a,b)
+soma2(b,a)
+
+
+
+
+
