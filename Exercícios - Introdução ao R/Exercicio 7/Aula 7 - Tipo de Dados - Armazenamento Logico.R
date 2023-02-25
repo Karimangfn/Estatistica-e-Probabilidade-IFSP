48cac5e3cb0a5906d263c7f81b50472941bbe323
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:19:27 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 7/Aula 7 - Tipo de Dados - Armazenamento Logico.R
@@ -0,0 +1,10 @@
+#Armazenamento Lógico
+
+Salario <- 3450.89
+Horas <- 220
+
+L1 <- Salario > Horas
+
+Logico <- TRUE
+Logico1 <- "TRUE"
+Logico2 <- c(1,TRUE,3) #A Linguagem R entende que TRUE equivale ao numero 1 e FALSE ao 0
