2b8665895879f63fd0e5771d7567888e10b0a176
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:19:03 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 5/Aula 5 - Tipos de Dados - Armazenamento de Caracteres.R
@@ -0,0 +1,10 @@
+#Armazenamento de Caracteres
+
+Nome_1 <- "Fernando Abreu"
+Nome_2 <- "Eduardo Maria"
+Idade <- "25"
+
+Nomes <- c(Nome_1, Nome_2)
+
+Nomes[1] #Nome na posição 1 do Array
+Nomes[2] #Nome na posição 2 do Array
