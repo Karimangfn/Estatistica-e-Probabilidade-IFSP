1659ae77a73b3518ca3ee12a1633f169350ec1a8
Kariman Gomes <86572034+Karimangfn@users.noreply.github.com>
2023-02-25 17:15:16 -0300

    Add files via upload

---
+++ Exercícios - Introdução ao R/Exercicio 1/Aula 1 - Conceitos Básicos.R
@@ -0,0 +1,10 @@
+#Como comentar em R -> Usar o #
+
+#Criando Variaveis
+#Variavel "a" recebeu 10, sinal <- para atribuição, a direção da seta é indiferente
+a <- 10  
+
+#Sinal "=" também funciona para atribuição
+b = 10
+
+#Como Limpar o Console -> Comando Control + L
